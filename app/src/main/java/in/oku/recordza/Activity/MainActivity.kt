fe35bc30cc8f314e64b615b2f47133498d082b82
Abhilash Chikara <abhilash.chikara@fastfox.com>
2018-09-17 16:38:01 +0530

    add code

--- app/src/main/java/in/oku/recordza/Activity/MainActivity.kt
+++ app/src/main/java/in/oku/recordza/Activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package `in`.oku.recordza.Activity
 
+import `in`.oku.recordza.Adaptor.DemoAdaptor
 import `in`.oku.recordza.R
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -7,19 +8,30 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.inflate_card_view.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), Animation.AnimationListener {
 
     var mAnimation: Animation? = null
+    var mList: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.inflate_card_view)
-        mAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_out)
-        mAnimation?.setAnimationListener(this)
-        mBackgroundIV.startAnimation(mAnimation)
+        setContentView(R.layout.activity_main)
+
+        for (i in 0..10) {
+            mList.add("ABHILASH CHIKARA")
+        }
+
+        val mAdaptor = DemoAdaptor(this, mList)
+        mRecyclerView.adapter = mAdaptor
+        mAdaptor.notifyDataSetChanged()
+
+
+//        mAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_out)
+//        mAnimation?.setAnimationListener(this)
+//        mBackgroundIV.startAnimation(mAnimation)
     }
 
     override fun onAnimationRepeat(animation: Animation?) {
--- app/src/main/java/in/oku/recordza/Adaptor/DemoAdaptor.kt
+++ app/src/main/java/in/oku/recordza/Adaptor/DemoAdaptor.kt
@@ -1,3 +1,72 @@
 package `in`.oku.recordza.Adaptor
 
-class DemoAdaptor
+import `in`.oku.recordza.R
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.inflate_card_view.view.*
+
+class DemoAdaptor(mContext: Context, mList: ArrayList<String>) :
+        RecyclerView.Adapter<DemoAdaptor.ViewHolderClass>() {
+
+    private var mContext: Context? = null
+    private var mList: ArrayList<String>? = null
+
+    init {
+        this.mContext = mContext
+        this.mList = mList
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass =
+            ViewHolderClass(LayoutInflater.from(mContext)
+                    .inflate(R.layout.inflate_card_view, parent,
+                            false))
+
+    override fun getItemCount(): Int = mList!!.size
+
+    override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+        holder.bindView()
+
+    }
+
+    inner class ViewHolderClass(itemView: View) : RecyclerView.ViewHolder(itemView),
+            Animation.AnimationListener {
+
+        var mBackgroundIV: ImageView? = null
+        var mTextView: TextView? = null
+
+        init {
+            this.mBackgroundIV = itemView.mBackgroundIV
+            this.mTextView = itemView.mTextView
+        }
+
+        fun bindView() {
+            val mAnimation = AnimationUtils.loadAnimation(mContext, R.anim.zoom_out)
+            mAnimation?.setAnimationListener(this)
+            mBackgroundIV!!.startAnimation(mAnimation)
+        }
+
+        override fun onAnimationRepeat(animation: Animation?) {
+        }
+
+        override fun onAnimationEnd(animation: Animation?) {
+        }
+
+        override fun onAnimationStart(animation: Animation?) {
+            val aniSlide = AnimationUtils.loadAnimation(mContext, R.anim.zoom_in)
+            itemView.mBackgroundIV?.startAnimation(aniSlide)
+
+            val aniText = AnimationUtils.loadAnimation(mContext, R.anim.flipin)
+            itemView.mTextView.startAnimation(aniText)
+
+        }
+
+    }
+
+}
